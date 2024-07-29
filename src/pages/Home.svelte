2e04d47e99eb4ccbf386461e4b868b9ca0ac4cde
OfentseXI <dofentse69@gmail.com>
2024-07-29 13:22:21 +0200

    Added FilterSort and ProductsList components to Home page

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,3 +2,10 @@
     import FilterSort from "../components/FilterSort.svelte";
     import ProductsList from "../components/ProductsList.svelte";
 </script>
+
+<main>
+    <div>
+        <FilterSort />
+    </div>
+    <ProductsList />
+</main>
