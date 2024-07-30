10999b0e3bb9430dbccd06baa3a222bc6685f156
OfentseXI <dofentse69@gmail.com>
2024-07-30 10:51:25 +0200

    Added product fetching functionality to ProductsList component

--- src/components/ProductsList.svelte
+++ src/components/ProductsList.svelte
@@ -1,5 +1,27 @@
 <script>
   import { onMount } from 'svelte';
+
+  let products = [];
+  let loading = true;
+  let error = null;
+
+  async function getProducts() {
+  try {
+    let response = await fetch('https://fakestoreapi.com/products');
+    if (!response.ok) {
+      throw new error("Failed to fetch products");
+    }
+    products = await response.json();
+  } catch (err) {
+    error = err.message;
+  } finally {
+    loading = false;
+  }
+}
+
+onMount(() => {
+  getProducts();
+});
 </script>
 <style></style>
 <main>
